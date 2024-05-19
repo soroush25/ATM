f32f66fb98db8a25014c70f3d2c6ef1691e14287
soroush25 <soroosh1480@gmail.com>
2024-05-19 20:23:41 +0330

    Add files via upload

--- mft/model/da/db.sql
+++ mft/model/da/db.sql
@@ -14,21 +14,25 @@ create table admin(
                       gender varchar2(6),
                       birth_date date,
                       city varchar2(20),
-                      permission nvarchar2(30)
+                      permission nvarchar2(10)
 );
 
 create table account(
                         accountNumber number primary key,
                         balance number,
-                        customer nvarchar2(30)
+                        customer nvarchar2(30),
+                        transaction nvarchar2(20),
+                        bank nvarchar2(10),
+                        accountTypes nvarchar2(10)
 );
 
 create table transaction(
-                         id number primary key ,
-                         amount nvarchar2(30),
-                         deposit nvarchar2(30),
-                         on_date date,
-                         Account varchar2(20)
+                            id number primary key ,
+                            amount nvarchar2(30),
+                            deposit nvarchar2(30),
+                            transactionDateAndTime date,
+                            account varchar2(20),
+                            transactionType nvarchar2(10)
 );
 
 create sequence customer_seq  start with 1 increment by 1;
