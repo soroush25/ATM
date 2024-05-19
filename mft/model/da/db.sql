793512b40a3bfe809eb8651d753aff4734876e0e
soroush25 <soroosh1480@gmail.com>
2024-05-19 20:01:57 +0330

    Add files via upload

--- mft/model/da/db.sql
+++ mft/model/da/db.sql
@@ -1,13 +1,37 @@
 create table customer(
-    id number primary key ,
-    name nvarchar2(30),
-    family nvarchar2(30),
-    gender varchar2(6),
-    birth_date date,
-    city varchar2(20),
-    algo number(1),
-    se number(1),
-    ee number(1)
+                         id number primary key,
+                         name nvarchar2(30),
+                         family nvarchar2(30),
+                         gender varchar2(6),
+                         birth_date date,
+                         city varchar2(20)
 );
 
-create sequence person_seq  start with 1 increment by 1;
+create table admin(
+                      id number primary key,
+                      name nvarchar2(30),
+                      family nvarchar2(30),
+                      gender varchar2(6),
+                      birth_date date,
+                      city varchar2(20),
+                      permission nvarchar2(30)
+);
+
+create table account(
+                        accountNumber number primary key,
+                        balance number,
+                        customer nvarchar2(30)
+);
+
+create table transaction(
+                         id number primary key ,
+                         amount nvarchar2(30),
+                         deposit nvarchar2(30),
+                         on_date date,
+                         Account varchar2(20)
+);
+
+create sequence customer_seq  start with 1 increment by 1;
+create sequence admin_seq  start with 1 increment by 1;
+create sequence account_seq  start with 1 increment by 1;
+create sequence transaction_seq  start with 1 increment by 1;
