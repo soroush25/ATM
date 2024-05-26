1edf5b06ccebe0899e3afce13b101c9b7db3fe33
imsammm <samabedi372@gmail.com>
2024-05-26 19:59:12 +0330

    Add files via upload

--- src/model/tools/CRUD.java
+++ src/model/tools/CRUD.java
@@ -1,5 +1,7 @@
 package src.model.tools;
 
+import src.model.entity.Account;
+
 import java.util.List;
 
 public interface CRUD<T> {
@@ -8,4 +10,10 @@ public interface CRUD<T> {
     T remove(int id) throws Exception;
     List<T> findAll()throws Exception;
     T findById(int id)throws Exception;
+
+    List<Account> findByCustomerId(String customer) throws Exception;
+
+    List<Account> findByBankName(String customer) throws Exception;
+
+    List<Account> findByAccountType(String customer) throws Exception;
 }
