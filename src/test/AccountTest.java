a89f6a7dc307a78246970edc65214848f2a380d2
soroush <soroosh1480@gmail.com>
2024-06-01 12:33:39 +0330

    Edited By Soroush

--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -1,16 +1,11 @@
 package src.test;
 
 import src.model.bl.AccountBl;
-import src.model.bl.AdminBl;
 import src.model.entity.Account;
-import src.model.entity.Admin;
 import src.model.entity.Customer;
 import src.model.entity.enums.BankAccountTypes;
 import src.model.entity.enums.Banks;
 import src.model.entity.enums.City;
-import src.model.entity.enums.Gender;
-
-import java.time.LocalDate;
 
 public class AccountTest {
     public static void main(String[] args) throws Exception {
@@ -33,11 +28,11 @@ public class AccountTest {
 
         Account account = Account
                 .builder()
-                .accountNumber(1232)
-                .balance(234)
+                .accountNumber(1)
+                .balance(1)
                 .customer(customer)
                 .bank(Banks.Meli)
-                .accountTypes(BankAccountTypes.SavingAccount)
+                .accountTypes(BankAccountTypes.Saving)
                 .build();
         System.out.println(AccountBl.getAccountBl().save(account));
     }
