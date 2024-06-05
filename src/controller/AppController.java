cee63f228a879f14d8b197784001bc2ad709cc8e
soroush <soroosh1480@gmail.com>
2024-06-05 15:38:21 +0330

    Edited By Soroush

--- src/controller/AppController.java
+++ src/controller/AppController.java
@@ -53,7 +53,7 @@ public class AppController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        log.info("Startup");
+        log.info("Start");
         for (City value : City.values()) {
             citycmb.getItems().add(value.name());
         }
