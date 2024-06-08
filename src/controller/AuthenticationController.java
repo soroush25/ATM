b7d7a0cddf36d9552707916c223298a7f0acf7af
soroush <soroosh1480@gmail.com>
2024-06-08 16:59:26 +0330

    Edited By Soroush

--- src/controller/AuthenticationController.java
+++ src/controller/AuthenticationController.java
@@ -1,29 +1,17 @@
 package src.controller;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import lombok.extern.log4j.Log4j;
 import src.model.bl.AdminBl;
 import src.model.bl.CustomerBl;
-import src.model.da.CustomerDa;
-import src.model.entity.Account;
-import src.model.entity.Admin;
-import src.model.entity.Customer;
-import src.model.entity.enums.City;
-import src.model.entity.enums.Gender;
-import src.model.tools.Validator;
 import src.view.WindowsManager;
 
 import java.net.URL;
-import java.util.Collections;
-import java.util.List;
 import java.util.ResourceBundle;
 
 @Log4j
