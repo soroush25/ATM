2c6ed95ae8b2fe6c01c33574486240f795b256d9
soroush <soroosh1480@gmail.com>
2024-06-10 13:03:55 +0330

    Edited By Soroush

--- src/controller/AdminAccountController.java
+++ src/controller/AdminAccountController.java
@@ -24,31 +24,16 @@ import java.util.ResourceBundle;
 @Log4j
 public class AdminAccountController implements Initializable {
     @FXML
-    private TextField adminSearchField, fnameField, lnameField, nidField, emailField, phoneField, addressField, UsernameField, PasswordField, IdField;
+    private Button exit;
 
     @FXML
-    private Button exit, adminCreate, adminDelete, adminEdit;
+    private TableView<Admin> AdminAccountTbl;
 
     @FXML
-    private RadioButton maletoggle, femaletoggle;
+    private TableColumn<Admin, Integer> AccountNumberCol;
 
     @FXML
-    private TableView<Admin> adminTable;
-
-    @FXML
-    private TableColumn<Admin, Integer> adminTableAccountNumber;
-
-    @FXML
-    private TableColumn<Admin, String> adminTableAccountName, adminTableAccountBalance, adminTableAccountType;
-
-    @FXML
-    private ToggleGroup genderToggle;
-
-    @FXML
-    private ComboBox<String> cityCmb;
-
-    @FXML
-    private DatePicker birthDatePicker;
+    private TableColumn<Admin, String> AccountNameCol, AccountBalanceCol, AccountTypeCol;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -57,77 +42,9 @@ public class AdminAccountController implements Initializable {
         try {
             resetForm();
         } catch (Exception e) {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "Admin Error\n" + e.getMessage());
+            Alert alert = new Alert(Alert.AlertType.ERROR, "AdminAccount Error\n" + e.getMessage());
             alert.show();
         }
-
-        adminCreate.setOnAction(event -> {
-            try {
-                RadioButton gender = (RadioButton) genderToggle.getSelectedToggle();
-                Customer customer = new Customer()
-                        .builder()
-                        .id(Integer.parseInt(IdField.getText()))
-                        .firstName(Validator.nameValidator(fnameField.getText(), "Invalid First Name!"))
-                        .lastName(Validator.nameValidator(lnameField.getText(), "Invalid Last Name!"))
-                        .nationalId(Validator.nationalIDValidator(nidField.getText(), "Invalid National ID!"))
-                        .gender(Gender.valueOf(gender.getText()))
-                        .birthDate(birthDatePicker.getValue())
-                        .email(Validator.emailValidator(emailField.getText(), "Invalid Email!"))
-                        .phone(Validator.phoneValidator(phoneField.getText(), "Invalid Phone!"))
-                        .address(Validator.addressValidator(addressField.getText(), "Invalid Address!"))
-                        .username(UsernameField.getText())
-                        .password(PasswordField.getText())
-                        .city(City.valueOf(cityCmb.getSelectionModel().getSelectedItem()))
-                        .build();
-                CustomerBl.getCustomerBl().save(customer);
-                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Saved!");
-                alert.show();
-                resetForm();
-            } catch (Exception e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR, "Error!\n" + e.getMessage());
-                alert.show();
-            }
-        });
-
-        adminEdit.setOnAction(event -> {
-            try {
-                RadioButton gender = (RadioButton) genderToggle.getSelectedToggle();
-                Customer customer = new Customer()
-                        .builder()
-                        .id(Integer.parseInt(IdField.getText()))
-                        .firstName(Validator.nameValidator(fnameField.getText(), "Invalid First Name!"))
-                        .lastName(Validator.nameValidator(lnameField.getText(), "Invalid Last Name!"))
-                        .nationalId(Validator.nationalIDValidator(nidField.getText(), "Invalid National ID!"))
-                        .gender(Gender.valueOf(gender.getText()))
-                        .birthDate(birthDatePicker.getValue())
-                        .email(Validator.emailValidator(emailField.getText(), "Invalid Email!"))
-                        .phone(Validator.phoneValidator(phoneField.getText(), "Invalid Phone!"))
-                        .address(Validator.addressValidator(addressField.getText(), "Invalid Address!"))
-                        .username(UsernameField.getText())
-                        .password(PasswordField.getText())
-                        .city(City.valueOf(cityCmb.getSelectionModel().getSelectedItem()))
-                        .build();
-                CustomerBl.getCustomerBl().edit(customer);
-                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Saved!");
-                alert.show();
-                resetForm();
-            } catch (Exception e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR, "Error!\n" + e.getMessage());
-                alert.show();
-            }
-        });
-
-        adminDelete.setOnAction(event -> {
-            try {
-                CustomerBl.getCustomerBl().remove(Integer.parseInt(IdField.getText()));
-                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Deleted!");
-                alert.show();
-                resetForm();
-            } catch (Exception e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR, "Error!\n" + e.getMessage());
-                alert.show();
-            }
-        });
 
         exit.setOnAction((event -> {
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Quit?");
@@ -136,61 +53,18 @@ public class AdminAccountController implements Initializable {
             }
             log.info("Quited");
         }));
-
-        adminSearchField.setOnKeyReleased((event) -> {
-            try {
-                showDataOnTable(Collections.singletonList(AdminBl.getAdminBl().findById(Integer.parseInt(adminSearchField.getText()))));
-                showDataOnTable(AdminBl.getAdminBl().findByFamily(adminSearchField.getText()));
-                showDataOnTable(Collections.singletonList(AdminBl.getAdminBl().findByNationalId(adminSearchField.getText())));
-                showDataOnTable(Collections.singletonList(AdminBl.getAdminBl().findByUsername(adminSearchField.getText())));
-            } catch (Exception e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR, "Error: \n" + e.getMessage());
-                alert.show();
-                log.error("Search Error: " + e.getMessage());
-            }
-        });
-
-        adminTable.setOnMouseClicked((event) -> {
-            Admin admin = adminTable.getSelectionModel().getSelectedItem();
-            IdField.setText(String.valueOf(admin.getId()));
-            fnameField.setText(admin.getFirstName());
-            lnameField.setText(admin.getLastName());
-            nidField.setText(admin.getNationalId());
-            if (admin.getGender().equals(Gender.Male)) {
-                maletoggle.setSelected(true);
-            } else {
-                femaletoggle.setSelected(true);
-            }
-            birthDatePicker.setValue(admin.getBirthDate());
-            emailField.setText(admin.getEmail());
-            phoneField.setText(admin.getPhone());
-            cityCmb.getSelectionModel().select(admin.getCity().ordinal());
-            addressField.setText(admin.getAddress());
-        });
     }
 
     private void showDataOnTable(List<Admin> customerList) throws Exception {
         ObservableList<Admin> observableList = FXCollections.observableList(customerList);
-        adminTableAccountNumber.setCellValueFactory(new PropertyValueFactory<>("accountNumber"));
-        adminTableAccountName.setCellValueFactory(new PropertyValueFactory<>("lname"));
-        adminTableAccountBalance.setCellValueFactory(new PropertyValueFactory<>("balance"));
-        adminTableAccountType.setCellValueFactory(new PropertyValueFactory<>("accountType"));
-        adminTable.setItems(observableList);
+        AccountNumberCol.setCellValueFactory(new PropertyValueFactory<>("accountNumber"));
+        AccountNameCol.setCellValueFactory(new PropertyValueFactory<>("lname"));
+        AccountBalanceCol.setCellValueFactory(new PropertyValueFactory<>("balance"));
+        AccountTypeCol.setCellValueFactory(new PropertyValueFactory<>("accountType"));
+        AdminAccountTbl.setItems(observableList);
     }
 
     private void resetForm() throws Exception {
-        IdField.clear();
-        fnameField.clear();
-        lnameField.clear();
-        nidField.clear();
-        maletoggle.setSelected(true);
-        emailField.clear();
-        phoneField.clear();
-        addressField.clear();
-        UsernameField.clear();
-        PasswordField.clear();
-        birthDatePicker.setValue(null);
-        cityCmb.getSelectionModel().select(0);
         showDataOnTable(AdminBl.getAdminBl().findAll());
     }
 }
